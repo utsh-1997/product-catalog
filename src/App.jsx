9d8848dd615148f534507e670b2680c4d7bee32c
utsh-1997 <utsh1997@gmail.com>
2026-04-28 12:15:19 +0530

    Day 1: Project setup and first ProductCard component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ProductCard from "./components/ProductCard"; 
+
+function App() {
+  return(
+    <div>
+      <h1>Product Catalog</h1>
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  ); 
+}
+
+export default App;
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,12 @@
+function ProductCard() {
+    return(
+        <div className="product-card">
+            <h2>Wireless Earbuds</h2>
+            <p className="category">Electronics</p>
+            <p className="price">7000/-</p>
+            <button>View Details</button>
+        </div>
+    );
+}
+
+export default ProductCard;

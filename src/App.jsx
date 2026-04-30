7b312554b161955ffcbe5b25f89ec41ba81abef5
utsh-1997 <utsh1997@gmail.com>
2026-04-30 11:32:17 +0530

    Day 3: Connected React to PHP MySQL backend via useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ProductCard from './components/ProductCard';
 
-const products = [
-  { id: 1, name: "Wireless Earbuds", price: "7000", category: "Electronics" },
-  { id: 2, name: "Running Shoes", price: "3500", category: "Footwear" },
-  { id: 3, name: "Water Bottle", price: "499", category: "Sports" },
-  { id: 4, name: "Laptop Stand", price: "1299", category: "Accessories" },
-  { id: 5, name: "Sunglasses", price: "2199", category: "Fashion" },
-  { id: 6, name: "Yoga Mat", price: "899", category: "Fitness" },
-  { id: 7, name: "Bracelet", price: "107000", category: "Jwellery" },
-];
-
 function App() {
+  const [products, setProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost/product-api/products.php')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(err => console.log(err));
+  }, []);
 
   const filteredProducts = products.filter((product) =>
     product.name.toLowerCase().includes(searchTerm.toLowerCase())
